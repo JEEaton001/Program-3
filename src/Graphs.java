5932212db9803c2c671db77dc3c95c9d4984cac5
ScottGKirkpatrick <ScottGKirkpatrick@gmail.com>
2018-04-06 14:58:45 -0600

    changed comment

--- src/Graphs.java
+++ src/Graphs.java
@@ -1,13 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Author: James Eaton, Scott Kirkpatrick
+ * Date: 4/6/2018
+ * Ovwerview:
  */
 
-/**
- *
- * @author z79x595
- */
+
 public class Graphs {
     
 }
